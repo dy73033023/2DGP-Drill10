995d0aa15e135daadc76ab70437725d8a4ea98fe
dy73033023 <dy73033023@tukorea.ac.kr>
2025-11-03 20:12:29 +0900

    비둘기의 평균 비행속도 시속 65km 반영

--- bird.py
+++ bird.py
@@ -2,10 +2,15 @@ from pico2d import *
 import game_framework
 import random
 
-PIXEL_PER_METER = (1.0 / 0.03) # 1pixel = 3cm, 1m = 33.33 pixel
 flip = ' '
 
-# Bird fly Speed
+# Bird의 fly Speed 계산
+pixels_per_meter = (10.0 / 0.3)  # 10 pixel 30 cm
+fly_speed_kmph = 65  # Km / Hour 비둘기 시속 50 ~ 80 km/h
+fly_speed_mpm = (fly_speed_kmph * 1000.0 / 60.0)
+fly_speed_mps = (fly_speed_mpm / 60.0)
+fly_speed_pps = (fly_speed_mps * pixels_per_meter) # 초당 픽셀 이동 거리 -> 약 18m -> 약 600 픽셀
+
 TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 5
@@ -13,10 +18,10 @@ FRAMES_PER_ACTION = 5
 class Bird:
     image = None
 
-    def __init__(self, x=400, y=300, velocity = 1):
+    def __init__(self):
         if Bird.image is None:
             Bird.image = load_image('bird_animation.png')
-        self.x, self.y, self.velocity = x, y, velocity
+        self.x, self.y = (random.randint(100, 1500),random.randint(100, 500))
         self.frame = 0.0
 
     def draw(self):
@@ -26,7 +31,7 @@ class Bird:
             180, 168,         # width, height
             0, flip,            # 회전 없음, 플립 없음
             self.x, self.y,   # 화면 중심
-            PIXEL_PER_METER / 3 * 2, PIXEL_PER_METER / 3 * 2 # 비둘기 크기 66cm -> 22~24 pixel
+            pixels_per_meter / 3 * 2, pixels_per_meter / 3 * 2 # 비둘기 크기 66cm -> 22~24 pixel
         )
 
     def update(self):
@@ -35,9 +40,9 @@ class Bird:
         self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 5
 
         if flip == ' ':
-            self.x += self.velocity
+            self.x += fly_speed_pps
         elif flip == 'h':
-            self.x -= self.velocity
+            self.x -= fly_speed_pps
 
         if self.x > 1500:
             flip = 'h'

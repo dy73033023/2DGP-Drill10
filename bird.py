99a71e85b2a63fa555f3857487dd47855f97d5fd
dy73033023 <dy73033023@tukorea.ac.kr>
2025-11-03 19:30:40 +0900

    프레임당 액션 횟수 지정, 속도 설정, 출력 구현, 방향 전환 구현

--- bird.py
+++ bird.py
@@ -1,20 +1,40 @@
 from pico2d import *
-import game_world
 import game_framework
-import random
 
 PIXEL_PER_METER = (1.0 / 0.03) # 1pixel = 3cm, 1m = 33.33 pixel
+flip = ' '
+
+# Boy Run Speed
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 5
 
 class Bird:
     image = None
-    def __init__(self, x = 400, y = 300):
-        if Bird.image == None:
+
+    def __init__(self, x=400, y=300, velocity = 1):
+        if Bird.image is None:
             Bird.image = load_image('bird_animation.png')
-        self.x, self.y = x, y
-        self.frame = 0
+        self.x, self.y, self.velocity = x, y, velocity
+        self.frame = 0.0
 
     def draw(self):
+        idx = int(self.frame) % 5
+        self.image.clip_composite_draw(
+            idx * 180,  168 * 2,   # left, bottom (맨 윗줄)
+            180, 168,         # width, height
+            0, flip,            # 회전 없음, 플립 없음
+            self.x, self.y,   # 화면 중심
+            200, 200          # 출력 크기 (잘림 방지)
+        )
 
     def update(self):
+        global flip
 
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 5
 
+        if self.x > 1400:
+            self.x -= self.velocity
+            flip = 'h'
+        else:
+            self.x += self.velocity
